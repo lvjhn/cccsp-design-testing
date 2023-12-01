daecc1b8b1f1ccea13c415bd2e5618d51f544841
lvjhn.d@gmail.com <lvjhn.d@gmail.com>
2023-12-02 03:52:48 +0800

    Deploy testing 5

--- utils/publish.sh
+++ utils/publish.sh
@@ -2,7 +2,8 @@ source backend/env/bin/activate
 python3 -m pip freeze > backend/requirements.txt
 cd frontend 
 yarn build 
-mv dist ../backend/dist
+rm ../backend/dist
+mv dist ../backend/
 cd ..
 git add .
 git commit -m "$1"
