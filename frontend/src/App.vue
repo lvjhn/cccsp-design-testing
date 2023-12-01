006669afa8927f77e5c6bfc006f55421dca4b269
lvjhn.d@gmail.com <lvjhn.d@gmail.com>
2023-12-02 04:07:02 +0800

    Deploy 6

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,16 +3,71 @@
   import { defineForMobile } from './helpers/uiScreenshotSize';
   import { useGenericModal } from '@/composables/generic-modal.js'
   import GenericModal from './components/generic-modal/GenericModal.vue';
+  import { ref } from 'vue';
 
   let modal = useGenericModal();
 
   modal.setModal(TaskViewerModal, {});
 
+  let imagesLoaded = ref(0);   
+  let mobileImages = []; 
+  let webImages = []; 
+
+  for(let i = 1; i <= 20; i++) {
+    mobileImages.push("/images/ui-screenshots/mobile/mobile(" + i + ").png")
+  }
+
+  for(let i = 1; i <= 15; i++) {
+    webImages.push("/images/ui-screenshots/web/web(" + i + ").png")
+  }
+
+
+  let imagesToLoad = mobileImages.length + webImages.length;
+
+  function preloadImages(urls, allImagesLoadedCallback){
+      var loadedCounter = 0;
+      var toBeLoadedNumber = urls.length;
+      urls.forEach(function(url){
+        preloadImage(url, function(){
+            loadedCounter++;
+                console.log('Number of loaded images: ' + loadedCounter);
+          if(loadedCounter == toBeLoadedNumber){
+            allImagesLoadedCallback();
+          }
+          imagesLoaded.value += 1;
+        });
+      });
+
+      function preloadImage(url, anImageLoadedCallback){
+          var img = new Image();
+          img.onload = anImageLoadedCallback;
+          img.src = url;
+      }
+  }
+
+
+
+  preloadImages([
+    ...mobileImages, 
+    ...webImages
+  ], function(){
+      console.log('All images were loaded');
+  });
+
 </script>
 
 <template>
   <div class="app-component"> 
-    <router-view />
+    <div class="center-fixed loading-screen" v-if="imagesLoaded < imagesToLoad"> 
+      Please wait while we are preloading images... 
+      <br />
+      This will make the activity experience smoother later.
+      <br /> 
+      <b>Images Loaded</b>: {{ imagesLoaded }} of {{ imagesToLoad }}
+    </div>
+    <div v-else>
+      <router-view />
+    </div>
   </div>
   <GenericModal />
 </template>
